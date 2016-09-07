cf62b871f818f77d875b0b8b36ceffb1b82faf24
rorellana2 <romarioorellana949@gmail.com>
2016-09-06 17:30:43 -0700

    Final App (Yarr World2)

---
+++ app/src/main/java/edu/orangecoastcollege/cs273/yarrworld2/MainActivity.java
@@ -0,0 +1,44 @@
+package edu.orangecoastcollege.cs273.yarrworld2;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Define the button as an instance (member variable)
+
+    Button speakButton;
+    Context context = this;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        // Restoring user settings from a previous session
+        super.onCreate(savedInstanceState);
+        // Inflating (!) opening the layout called activity_main (activity_main.xml)
+        setContentView(R.layout.activity_main);
+
+        // Hook up the speakButton to the one in our view:
+        speakButton = (Button) findViewById(R.id.speakButton);
+
+        // Wire up the speakButton to perform an onClick event
+        speakButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        // Use the static reference to "Toast" to display the message:
+                        // Toast = vanishes after set amount of time
+                        // Dialog = requires user input
+
+                        //
+                        Toast.makeText(context, R.string.toast_text, Toast.LENGTH_LONG).show();
+
+                    }
+                }
+        );
+    }
+}
